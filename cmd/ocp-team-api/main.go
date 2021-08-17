45092b7719a9d950f94fb4a6a1aa1788e4b9aabe
Your Name <i.vahula@innopolis.ru>
2021-08-17 13:50:18 +0300

    protofiles and grpc service were added

--- cmd/ocp-team-api/main.go
+++ cmd/ocp-team-api/main.go
@@ -1,36 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/ozoncp/ocp-team-api/internal/api"
+	desc "github.com/ozoncp/ocp-team-api/pkg/ocp-team-api"
 )
 
-func main() {
-	fmt.Println("The ocp-team-api project.")
+const (
+	grpcPort = ":8082"
+	grpcServerEndpoint = "localhost:8082"
+	httpPort = ":8080"
+)
 
-	for i := 0; i < 5; i++ {
-		if err := ReadFile("go.mod"); err != nil {
-			fmt.Println(err)
-			break
-		}
+func runJSON() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := desc.RegisterOcpTeamApiHandlerFromEndpoint(ctx, mux, grpcServerEndpoint, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	err = http.ListenAndServe(httpPort, mux)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func ReadFile(path string) (err error) {
-	file, err := os.Open(path)
-
+func runGrpc() error {
+	listen, err := net.Listen("tcp", grpcPort)
 	if err != nil {
-		return
+		log.Fatalf("failed to listen: %v", err)
 	}
 
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			return
-		} else {
-			fmt.Printf("File %s successfully closed\n", file.Name())
-		}
-	}(file)
+	s := grpc.NewServer()
+	desc.RegisterOcpTeamApiServer(s, api.NewOcpTeamApi())
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
 	return nil
+}
+
+
+func main() {
+	go runJSON()
+
+	if err := runGrpc(); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/api/api.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"context"
+	desc "github.com/ozoncp/ocp-team-api/pkg/ocp-team-api"
+	log "github.com/rs/zerolog/log"
+)
+
+type api struct {
+	desc.UnimplementedOcpTeamApiServer
+}
+
+func NewOcpTeamApi() desc.OcpTeamApiServer {
+	return &api{}
+}
+
+func (a *api) CreateTeamV1(
+	ctx context.Context,
+	req *desc.CreateTeamV1Request) (*desc.CreateTeamV1Response, error) {
+	log.Printf("Create team (name=%s, description=%s)", req.Name, req.Description)
+	return &desc.CreateTeamV1Response{}, nil
+}
+
+func (a *api) DescribeTeamV1(
+	ctx context.Context,
+	req *desc.DescribeTeamV1Request) (*desc.DescribeTeamV1Response, error) {
+	log.Printf("Describe team (id=%d)", req.Id)
+	return &desc.DescribeTeamV1Response{}, nil
+}
+
+func (a *api) ListTeamsV1(
+	ctx context.Context,
+	req *desc.ListTeamsV1Request) (*desc.ListTeamsV1Response, error) {
+	log.Printf("List teams (limit=%d, offset=%d)", req.Limit, req.Offset)
+	return &desc.ListTeamsV1Response{}, nil
+}
+
+func (a *api) RemoveTeamV1(
+	ctx context.Context,
+	req *desc.RemoveTeamV1Request) (*desc.RemoveTeamV1Response, error) {
+	log.Printf("Remove team (id=%d)", req.Id)
+	return &desc.RemoveTeamV1Response{}, nil
+}
