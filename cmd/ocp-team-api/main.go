fecab181d435b8c86a188560f5bd523ec35b6c82
Igor Vakhula <darklinhamb@gmail.com>
2021-08-20 19:16:11 +0300

    graceful shutdown was added

--- cmd/ocp-team-api/main.go
+++ cmd/ocp-team-api/main.go
@@ -7,6 +7,10 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ozoncp/ocp-team-api/internal/api"
 	desc "github.com/ozoncp/ocp-team-api/pkg/ocp-team-api"
@@ -18,7 +22,7 @@ const (
 	httpPort = ":8080"
 )
 
-func runJSON() {
+func runJSON(stopSignal <-chan os.Signal, stopStruct chan<- struct{}) {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -31,13 +35,30 @@ func runJSON() {
 		panic(err)
 	}
 
-	err = http.ListenAndServe(httpPort, mux)
-	if err != nil {
-		panic(err)
+	s := &http.Server{
+		Addr: httpPort,
+		Handler: mux,
 	}
+
+	go func() {
+		err = s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	<-stopSignal
+	shutdownCtx, shutdownCancel := context.WithTimeout(ctx, 5 * time.Second)
+	defer shutdownCancel()
+
+	if err = s.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("server shutdown failed:%+v", err)
+	}
+
+	stopStruct<- struct{}{}
 }
 
-func runGrpc() error {
+func runGrpc(stopStruct <-chan struct{}) error {
 	listen, err := net.Listen("tcp", grpcPort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -46,18 +67,27 @@ func runGrpc() error {
 	s := grpc.NewServer()
 	desc.RegisterOcpTeamApiServer(s, api.NewOcpTeamApi())
 
-	if err := s.Serve(listen); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		if err := s.Serve(listen); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-stopStruct
+	s.GracefulStop()
 
 	return nil
 }
 
-
 func main() {
-	go runJSON()
+	stopGateway := make(chan os.Signal, 1)
+	stopGrpcServer := make(chan struct{}, 1)
 
-	if err := runGrpc(); err != nil {
+	signal.Notify(stopGateway, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go runJSON(stopGateway, stopGrpcServer)
+
+	if err := runGrpc(stopGrpcServer); err != nil {
 		log.Fatal(err)
 	}
 }
