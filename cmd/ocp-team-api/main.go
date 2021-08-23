48d0e9c7e90d847f838836e6fe41a81c01c57aa9
Igor Vakhula <darklinhamb@gmail.com>
2021-08-23 10:56:52 +0300

    graceful shutdown updated

--- cmd/ocp-team-api/main.go
+++ cmd/ocp-team-api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/ozoncp/ocp-team-api/internal/api"
+	desc "github.com/ozoncp/ocp-team-api/pkg/ocp-team-api"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -11,22 +14,32 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/ozoncp/ocp-team-api/internal/api"
-	desc "github.com/ozoncp/ocp-team-api/pkg/ocp-team-api"
 )
 
 const (
-	grpcPort = ":8082"
+	grpcPort           = ":8082"
 	grpcServerEndpoint = "localhost:8082"
-	httpPort = ":8080"
+	httpPort           = ":8080"
 )
 
-func runJSON(stopSignal <-chan os.Signal, stopStruct chan<- struct{}) {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+var (
+	grpcServer  *grpc.Server
+	httpGateway *http.Server
+)
 
+func runGrpcServer() error {
+	listen, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer = grpc.NewServer()
+	desc.RegisterOcpTeamApiServer(grpcServer, api.NewOcpTeamApi())
+
+	return grpcServer.Serve(listen)
+}
+
+func runHttpGateway(ctx context.Context) error {
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 
@@ -35,59 +48,54 @@ func runJSON(stopSignal <-chan os.Signal, stopStruct chan<- struct{}) {
 		panic(err)
 	}
 
-	s := &http.Server{
-		Addr: httpPort,
+	httpGateway = &http.Server{
+		Addr:    httpPort,
 		Handler: mux,
 	}
 
-	go func() {
-		err = s.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	<-stopSignal
-	shutdownCtx, shutdownCancel := context.WithTimeout(ctx, 5 * time.Second)
-	defer shutdownCancel()
-
-	if err = s.Shutdown(shutdownCtx); err != nil {
-		log.Fatalf("server shutdown failed:%+v", err)
+	if err = httpGateway.ListenAndServe(); err != http.ErrServerClosed {
+		return err
 	}
-
-	stopStruct<- struct{}{}
-}
-
-func runGrpc(stopStruct <-chan struct{}) error {
-	listen, err := net.Listen("tcp", grpcPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	s := grpc.NewServer()
-	desc.RegisterOcpTeamApiServer(s, api.NewOcpTeamApi())
-
-	go func() {
-		if err := s.Serve(listen); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	}()
-
-	<-stopStruct
-	s.GracefulStop()
 
 	return nil
 }
 
 func main() {
-	stopGateway := make(chan os.Signal, 1)
-	stopGrpcServer := make(chan struct{}, 1)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 
-	signal.Notify(stopGateway, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
-	go runJSON(stopGateway, stopGrpcServer)
+	g, ctx := errgroup.WithContext(ctx)
 
-	if err := runGrpc(stopGrpcServer); err != nil {
+	g.Go(runGrpcServer)
+	g.Go(func() error { return runHttpGateway(ctx) })
+
+	select {
+	case <-interrupt:
+		break
+	case <- ctx.Done():
+		break
+	}
+
+	log.Println("received shutdown signal")
+
+	cancel()
+
+	shutdownCtx, shutdownCtxCancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer shutdownCtxCancel()
+
+	if httpGateway != nil {
+		_ = httpGateway.Shutdown(shutdownCtx)
+		log.Println("shutdown http gateway")
+	}
+	if grpcServer != nil {
+		grpcServer.GracefulStop()
+		log.Println("shutdown grpc server")
+	}
+
+	if err := g.Wait(); err != nil {
 		log.Fatal(err)
 	}
 }
